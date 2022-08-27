c527703200b2da9ad07fa631b4cac72cbfcfd6fb
niveK77pur <kevinbiewesch@yahoo.fr>
2022-08-27 20:38:09 +0200

    set up multiple cursor plugin

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -33,6 +33,24 @@ return require('packer').startup({function(use)
             })
         end, -- }}}
     }
+
+    use { 'mg979/vim-visual-multi', branch = 'master',
+        config = function() -- {{{
+            vim.g.VM_leader = [[\]]
+            vim.g.VM_theme = 'iceblue'
+            vim.g.VM_mouse_mappings = 1
+            -- mouse mappings not working? :(
+            vim.keymap.set('n', '<C-LeftMouse>', '<Plug>(VM-Mouse-Cursor)')
+            vim.keymap.set('n', '<C-RightMouse>', '<Plug>(VM-Mouse-Word)')
+            vim.keymap.set('n', '<M-C-RightMouse>', '<Plug>(VM-Mouse-Column)')
+            vim.g.VM_maps = {
+                -- https://github.com/mg979/vim-visual-multi/wiki/Quick-start#undoredo
+                ["Undo"] = 'u',
+                ["Redo"] = '<C-r>',
+            }
+        end, -- }}}
+    }
+
     -- Interface --
     use 'sainnhe/everforest'
 end,
