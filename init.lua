daf76e50d18e34b83ac2f282112565d9c8e3c003
niveK77pur <kevinbiewesch@yahoo.fr>
2022-08-27 03:16:14 +0200

    add placeholder files

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ vim.keymap.set({ 'i', 'n' }, [[<Leader><Leader>]], [[<Leader>]])
 vim.keymap.set({ 'i', 'n' }, [[<LocalLeader><LocalLeader>]], [[<LocalLeader>]])
 
 local vimrc_files = {
+	'vimrc.functions',
+	'vimrc.mappings',
+	'vimrc.plugin-settings',
+	'vimrc.plugins',
 	'vimrc.settings',
 }
 
