99c568da7b3aa4898dccd21e39fd0b39fb739129
niveK77pur <kevinbiewesch@yahoo.fr>
2022-08-29 01:35:04 +0200

    ftplugin for markdown

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+local nmap = function(LH, RH, args) vim.keymap.set('n', LH, RH, args) end
+
+vim.opt.wrap = true
+
+for _,key in pairs{ 'j', 'k', '0', '$' } do
+    nmap(key, 'g'..key, { desc = string.format('remap %s for better text editing', key) })
+end
