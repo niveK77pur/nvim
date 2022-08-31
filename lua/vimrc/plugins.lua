71b2de95c15ad694842c420b442c5f6257b875d6
niveK77pur <kevinbiewesch@yahoo.fr>
2022-08-31 12:12:23 +0200

    pretty folds

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -497,6 +497,55 @@ return require('packer').startup({function(use)
 
     use { 'MunifTanjim/nui.nvim' }
 
+    use{ 'anuvyklack/pretty-fold.nvim',
+        config = function() -- {{{
+
+            local global_setup = {
+                sections = {
+                    left = { 'content', },
+                    right = {
+                        ' ',
+                        function() return ("[%dL]"):format(vim.v.foldend - vim.v.foldstart) end,
+                        '[', 'percentage', ']',
+                    }
+                },
+                matchup_patterns = {
+                    {  '{', '}' },
+                    { '%(', ')' }, -- % to escape lua pattern char
+                    { '%[', ']' }, -- % to escape lua pattern char
+                },
+            }
+
+            local function ft_setup(lang, options) -- {{{
+                local opts = vim.tbl_deep_extend('force', global_setup, options)
+                -- combine global and ft specific matchup_patterns
+                if opts and opts.matchup_patterns and global_setup.matchup_patterns then
+                    opts.matchup_patterns = vim.list_extend(opts.matchup_patterns, global_setup.matchup_patterns)
+                end
+                require('pretty-fold').ft_setup(lang, opts)
+            end -- }}}
+
+            require('pretty-fold').setup(global_setup)
+
+            ft_setup('lua', { -- {{{
+                matchup_patterns = {
+                    { '^%s*do$', 'end' }, -- do ... end blocks
+                    { '^%s*if', 'end' },  -- if ... end
+                    { '^%s*for', 'end' }, -- for
+                    { 'function%s*%w*%(', 'end' }, -- 'function( or 'function (''
+                },
+            }) -- }}}
+
+        end, -- }}}
+    }
+
+    use { 'anuvyklack/fold-preview.nvim',
+        requires = 'anuvyklack/keymap-amend.nvim',
+        config = function()
+            require('fold-preview').setup()
+        end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
