45b46367b53d2d7c8cbdef0c193c01d928b90338
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-04 14:50:34 +0200

    enable italics for embark theme

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -202,6 +202,9 @@ return require('packer').startup({function(use)
 
     use { 'embark-theme/vim',
         as = 'embark',
+        setup = function ()
+            vim.g.embark_terminal_italics = 1
+        end,
         config = function()
             vim.cmd('colorscheme embark')
         end
