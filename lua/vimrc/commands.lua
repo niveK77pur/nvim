742d63ad1de755f69694090015af76913aa854df
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-06 00:41:58 +0200

    commands for making titles

---
+++ lua/vimrc/commands.lua
@@ -0,0 +1,22 @@
+local vfn = require "vimrc.functions"
+
+vim.api.nvim_create_user_command('MakeTitle', function(opts)
+    print(("..%s.. ..%s.. ..%s.."):format(opts.count, opts.args, opts.fargs))
+    local titlefn
+    if opts.count == 1 then
+        titlefn = vfn.MakeHeader
+    elseif opts.count == 2 then
+        titlefn = vfn.MakeSection
+    else
+        titlefn = vfn.MakeSection
+    end
+
+    if opts.args and opts.args == '' then
+        -- query title with NUI if not specified with the command
+        vfn.nuiInput{ text = "Title", on_submit = function(title)
+            titlefn(title)
+        end }
+    else
+        titlefn(opts.args)
+    end
+end, { nargs = '*', count = 1, desc = 'Create title banners (:MakeTitle [<Level>] [<Text>])' })
