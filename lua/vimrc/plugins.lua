3abc8c1bd53a68ebd436ba7ad264ef564c19f8f2
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-06 00:04:42 +0200

    remove example colors from readme

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -235,15 +235,6 @@ return require('packer').startup({function(use)
                     -- PMenuThumb = { fg = colors.none,  bg = colors.Yellow },
 
                 },
-                -- custom_colors = { -- this table can hold any group of colors with their respective values
-                --     LineNr = { fg = "#5BBBDA", bg = "NONE", style = "NONE" },
-                --     CursorLineNr = { fg = "#E1CD6C", bg = "NONE", style = "NONE" },
-                --
-                --     -- it is possible to specify only the element to be changed
-                --     TelescopePreviewBorder = { fg = "#A13413" },
-                --     LspDiagnosticsDefaultError = { bg = "#E11313" },
-                --     TSTagDelimiter = { style = "bold,italic" },
-                -- }
             }
             end
     }
