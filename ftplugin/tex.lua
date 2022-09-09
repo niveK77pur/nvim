7a9256727beefa367dc277713f05cbd2c57fffa8
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-09 15:33:40 +0200

    change latex filetype settings

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,12 +1,18 @@
--- Settings --
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--                                   Settings
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 vim.bo.tabstop       = 2
 vim.bo.textwidth     = 70
 -- vim.bo.noexpandtab
+--
 -- vim.bo.keywordprg='texdoc'
+vim.g.vimtex_doc_handlers = { 'vimtex#doc#handlers#texdoc' }
+
 vim.wo.spell         = true
 -- allow concealing with vimtex
 vim.wo.conceallevel  = 2
 vim.wo.concealcursor = 'n'
 vim.bo.smartindent   = false
-vim.wo.foldenable    = false
+-- vim.wo.foldenable    = false
+
