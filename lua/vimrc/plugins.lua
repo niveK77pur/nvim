a2fb26d4d2eb41fab3b11a963f0e3f47ff6827e4
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-10 23:14:08 +0200

    new surround plugin

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -418,13 +418,55 @@ return require('packer').startup({function(use)
         end, -- }}}
     } -- }}}
 
-    use { 'tpope/vim-surround', disable=false,
+    use { 'tpope/vim-surround', disable=true,
         config = function()
             local map = vim.keymap.set
 
             vim.g.surround_no_mappings = true
             map('n', 'ds', '<Plug>Dsurround')
             map('n', 'cs', '<Plug>Csurround')
+        end
+    }
+    use { 'kylechui/nvim-surround', disable=false,
+        config = function()
+            require("nvim-surround").setup({
+                keymaps = {
+                    visual = '<Leader>s',
+                },
+                aliases = {
+                    ["a"] = false,
+                    ["b"] = false,
+                    ["B"] = false,
+                    ["r"] = false,
+                    ["s"] = false,
+                },
+                -- move_cursor = false,
+            })
+
+            local augroup_nvimsurround = vim.api.nvim_create_augroup('nvimsurround', {})
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                group = augroup_nvimsurround,
+                pattern = { 'lua' },
+                desc = "nvim-surround config for lua filetype",
+                callback = function()
+                    require('nvim-surround').buffer_setup {
+                        surrounds = {
+                            ['s'] = {
+                                add = { '[[', ']]' },
+                                find = "%[(=*)%[.*%]%1%]",
+                                -- find = function()
+                                --     local config = require("nvim-surround.config")
+                                --     -- return config.get_selection({ motion = '2a[' })
+                                --     return config.get_selection({ node = 'string' })
+                                -- end,
+                                delete = "^(%[=*%[)().*(%]=*%])()$",
+                                -- delete = '^(%[%[)().*(%]%])()$',
+                                change = { target =  "^(%[=*%[)().*(%]=*%])()$"},
+                            },
+                        }
+                    }
+                end,
+            })
         end
     }
 
