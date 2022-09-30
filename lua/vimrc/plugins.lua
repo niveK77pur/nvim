535673f65817daa57b99b1c09d62c5f62e25c96d
niveK77pur <kevinbiewesch@yahoo.fr>
2022-09-30 10:31:34 +0200

    add fancy automatic window resizing

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -835,6 +835,34 @@ return require('packer').startup({function(use)
         end,
     }
 
+    use { "anuvyklack/windows.nvim", disable=false,
+        requires = {
+            "anuvyklack/middleclass",
+            "anuvyklack/animation.nvim"
+        },
+        cmd = { 'WindowsEnableAutowidth', 'WindowsToggleAutowidth', 'WindowsMaximaze', },
+        config = function()
+            local width = 10
+            vim.o.winwidth = width
+            vim.o.winminwidth = width
+            vim.o.equalalways = false
+
+            require('windows').setup{
+                animation = {
+                    enable = true,
+                    duration = 150,
+                    -- fps = 30,
+                }
+            }
+
+            local nmap = function(LH, RH, args) vim.keymap.set('n', LH, RH, args) end
+            nmap('<Leader>we', '<cmd>WindowsEnableAutowidth<CR>')
+            nmap('<Leader>wd', '<cmd>WindowsDisableAutowidth<CR>')
+            nmap('<Leader>wt', '<cmd>WindowsToggleAutowidth<CR>')
+            nmap('<Leader>wm', '<cmd>WindowsMaximaze<CR>')
+        end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim {{{
     -- Put this at the end after all plugins
     if packer_bootstrap then
