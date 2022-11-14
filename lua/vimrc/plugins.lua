28fb3bca3c66153b53ae4925ae7a16f4977c5d16
niveK77pur <kevinbiewesch@yahoo.fr>
2022-11-14 11:55:46 +0100

    add silicon plugin (doesn't work?)

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -921,6 +921,22 @@ return require('packer').startup({function(use)
         end
     }
 
+    use { "narutoxy/silicon.lua", disable=false,
+        requires = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require('silicon').setup({})
+            -- local silicon = require('silicon')
+            -- Generate image of lines in a visual selection
+            vim.keymap.set('v', '<Leader>S',  function() silicon.visualise_api({ to_clip=true }) end )
+            -- Generate image of a whole buffer, with lines in a visual selection highlighted
+            -- vim.keymap.set('v', '<Leader>bs', function() silicon.visualise_api({to_clip = true, show_buf = true}) end )
+            -- Generate visible portion of a buffer
+            -- vim.keymap.set('n', '<Leader>s',  function() silicon.visualise_api({to_clip = true, visible = true}) end )
+            -- Generate current buffer line in normal mode
+            -- vim.keymap.set('n', '<Leader>s',  function() silicon.visualise_api({to_clip = true}) end )
+        end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim {{{
     -- Put this at the end after all plugins
     if packer_bootstrap then
