2cdd95811d031bc620209d121504139f23dffc81
niveK77pur <kevinbiewesch@yahoo.fr>
2022-11-15 17:05:44 +0100

    add SSR plugin

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -495,6 +495,27 @@ return require('packer').startup({function(use)
         end, -- }}}
     } -- }}}
 
+    use { "cshuaimin/ssr.nvim", disable=false,
+        module = "ssr",
+        setup = function()
+            -- ts : Treesitter Search-and-replace
+            vim.keymap.set({ "n", "x" }, "<Leader>ts", function() require("ssr").open() end)
+        end,
+        -- Calling setup is optional.
+        config = function()
+            require("ssr").setup {
+                min_width = 50,
+                min_height = 5,
+                keymaps = {
+                    close = "q",
+                    next_match = "n",
+                    prev_match = "N",
+                    replace_all = "<leader><cr>",
+                },
+            }
+        end
+    }
+
     -- Music --
 
     -- Language support --
