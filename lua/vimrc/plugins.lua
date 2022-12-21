b4391a4eaebabda32be0a8dce85480c6776a2222
niveK77pur <kevinbiewesch@yahoo.fr>
2022-12-22 00:44:02 +0100

    completion using ^y instead of ^M

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -963,7 +963,7 @@ return require('packer').startup({function(use)
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({
+                    ['<C-y>'] = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
                     }),
