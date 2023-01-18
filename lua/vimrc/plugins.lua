49bec040cbff0995f01b7db1176f597fd7854ddc
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-01-18 16:41:09 +0100

    use "new" bootstrap

--- lua/vimrc/plugins.lua
+++ lua/vimrc/plugins.lua
@@ -1,10 +1,16 @@
 -- install packer if not found {{{
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local packer_bootstrap
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 --  }}}
 -- automatically :PackerCompile when file is written {{{
 vim.cmd([[
