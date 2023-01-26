b604833777d7df28c9d25c0bfe0e25c673cf01e8
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-01-26 11:42:53 +0100

    configure trailblazer mappings

--- lua/myplugins/handy-tools/trailblazer.lua
+++ lua/myplugins/handy-tools/trailblazer.lua
@@ -2,7 +2,31 @@ return {
     "LeonHeidelbach/trailblazer.nvim",
     config = function()
         require("trailblazer").setup({
-            -- your custom config goes here
+            mappings = {
+                nv = { -- Mode union: normal & visual mode. Can be extended by adding i, x, ...
+                    motions = {
+                        new_trail_mark = '<A-l>',
+                        track_back = '<A-k>',
+                        peek_move_next_down = '<A-J>',
+                        peek_move_previous_up = '<A-K>',
+                        toggle_trail_mark_list = '<A-m>',
+                    },
+                    actions = {
+                        delete_all_trail_marks = '<A-L>',
+                        paste_at_last_trail_mark = '<A-p>',
+                        paste_at_all_trail_marks = '<A-P>',
+                        set_trail_mark_select_mode = '<A-t>',
+                    },
+                },
+                -- You can also add/move any motion or action to mode specific mappings i.e.:
+                -- i = {
+                --     motions = {
+                --         new_trail_mark = '<C-l>',
+                --         ...
+                --     },
+                --     ...
+                -- },
+            },
         })
     end,
 }
