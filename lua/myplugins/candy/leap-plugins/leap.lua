584ba297a64316d40b446e03554175c5c940857a
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-01-26 18:36:57 +0100

    make leap bi-directional

--- lua/myplugins/candy/leap-plugins/leap.lua
+++ lua/myplugins/candy/leap-plugins/leap.lua
@@ -11,5 +11,19 @@ return { 'ggandor/leap.nvim',
         --   function (win) return vim.api.nvim_win_get_config(win).focusable end,
         --   vim.api.nvim_tabpage_list_wins(0)
         -- )}
+
+        -- Bidirectional search. Initiate multi-window mode with the current
+        -- window as the only target.
+        vim.keymap.set(
+            { "n" }, "s",
+            function()
+                require('leap').leap({ target_windows = { vim.fn.win_getid() } })
+            end,
+            {
+                desc = "Bidirectional search. Initiate " ..
+                    "multi-window mode with the current window as the only " ..
+                    "target"
+            }
+        )
     end
 }
