8c9a6537a7b3621dee5a2e71c4596b6863b01404
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-03-27 11:36:15 +0200

    ts-node-action: add null-ls integration

--- lua/myplugins/editing/ts-node-action.lua
+++ lua/myplugins/editing/ts-node-action.lua
@@ -1,6 +1,6 @@
 return {
-    'ckolkey/ts-node-action',
-    dependencies = { 'nvim-treesitter' },
+    "ckolkey/ts-node-action",
+    dependencies = { "nvim-treesitter", "jose-elias-alvarez/null-ls.nvim" },
     config = function()
         require("ts-node-action").setup({})
         vim.keymap.set(
@@ -9,5 +9,13 @@ return {
             require("ts-node-action").node_action,
             { desc = "Trigger Node Action" }
         )
-    end
+        require("null-ls").register({
+            name = "more_actions",
+            method = { require("null-ls").methods.CODE_ACTION },
+            filetypes = { "_all" },
+            generator = {
+                fn = require("ts-node-action").available_actions,
+            },
+        })
+    end,
 }
