0129b03ee8b4d6bf62869d0deae8b72db1dc09bc
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-03-31 12:38:15 +0200

    deadcolumn: extra options

--- lua/myplugins/candy/deadcolumn.lua
+++ lua/myplugins/candy/deadcolumn.lua
@@ -3,11 +3,15 @@ return {
     enabled = true,
     event = { 'ColorScheme' },
     opts = {
+        modes = { 'i', 'n' },
         blending = {
             threshold = 2/3,
             colorcode = "#202330", -- pinkmare s:palette.bg0
         },
+        extra = {
+            follow_tw = "+0",
+        }
     },
 }
 
--- vim: cc=25
+-- vim: cc=25 tw=35
