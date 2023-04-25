9bfa268d682facf85ce243aedfeabc71680ff801
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-04-25 14:56:44 +0200

    apply formatter

--- lua/myplugins/language-support/treesitter-plugins/nvim-treesitter.lua
+++ lua/myplugins/language-support/treesitter-plugins/nvim-treesitter.lua
@@ -1,20 +1,19 @@
-return { 'nvim-treesitter/nvim-treesitter', disable=false,
+return {
+    "nvim-treesitter/nvim-treesitter",
+    disable = false,
     build = function()
-        require('nvim-treesitter.install').update({ with_sync = true })
-        vim.cmd[[TSUpdateSync]]
+        require("nvim-treesitter.install").update({ with_sync = true })
+        vim.cmd([[TSUpdateSync]])
     end,
     config = function()
-        require'nvim-treesitter.configs'.setup {
-
+        require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             -- ensure_installed = { "c", "lua", "rust" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
             -- Automatically install missing parsers when entering buffer
             auto_install = true,
-
             -- List of parsers to ignore installing (for "all")
             -- ignore_install = { "javascript" },
 
@@ -24,13 +23,14 @@ return { 'nvim-treesitter/nvim-treesitter', disable=false,
             highlight = {
                 enable = true,
                 -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is the name of the parser)
-                disable = { "latex", },
+                disable = {
+                    "latex",
+                },
             },
             incremental_selection = { enable = true },
             indent = { enable = false },
             -- textobjects = { enable = true },
-
-        }
+        })
 
         -- WORKAROUND
         -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
@@ -44,6 +44,5 @@ return { 'nvim-treesitter/nvim-treesitter', disable=false,
         })
         --]]
         -- ENDWORKAROUND
-
     end,
 }
