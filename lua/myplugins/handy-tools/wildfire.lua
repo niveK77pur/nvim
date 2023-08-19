d243c42058e5eb9d8fdb6e384912f000570e9a1a
niveK77pur <kevinbiewesch@yahoo.fr>
2023-08-19 16:49:31 +0200

    update wildfire (config)

--- lua/myplugins/handy-tools/wildfire.lua
+++ lua/myplugins/handy-tools/wildfire.lua
@@ -4,28 +4,11 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
         require('wildfire').setup({
-            -- surrounds = {
-            --     { '(', ')' },
-            --     { '{', '}' },
-            --     { '<', '>' },
-            --     { '[', ']' },
-            -- },
             keymaps = {
                 init_selection = '<CR>',
                 node_incremental = '<CR>',
                 node_decremental = '<BS>',
             },
-        })
-
-        local augroup_revert_cr = vim.api.nvim_create_augroup('revert_cr', {})
-
-        vim.api.nvim_create_autocmd({ 'FileType' }, {
-            group = augroup_revert_cr,
-            pattern = { 'qf' },
-            desc = "Revert wildfire's <CR> on filetypes",
-            callback = function()
-                vim.cmd('noremap <CR> <CR>')
-            end,
         })
     end,
 }
