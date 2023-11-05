8b7f487c048751ebee990728264b08e40092853c
niveK77pur <kevinbiewesch@yahoo.fr>
2023-11-06 00:40:27 +0100

    asciidoc: make section ID for github

--- ftplugin/asciidoc.lua
+++ ftplugin/asciidoc.lua
@@ -29,3 +29,10 @@ vim.api.nvim_buf_create_user_command(0, 'MDReplaceLink', function(args)
     )
 end, {range=1})
 
+vim.api.nvim_create_user_command('MDSectionID', function()
+    local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+    local title_line = vim.api.nvim_get_current_line()
+    local github_id = vim.fn.substitute(title_line, "\\v[[:space:]]+", [[-]], 'g')
+    local github_id = github_id:lower():match("^[=-]+(.*)")
+    vim.api.nvim_buf_set_lines(0, cursor_line-1, cursor_line-1, true, { string.format("[#%s]", github_id) })
+end, {})
