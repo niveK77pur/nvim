0a0708f58f91706cd3a84d2bab6d869c1f30d1f7
niveK77pur <kevinbiewesch@yahoo.fr>
2023-12-06 14:59:03 +0100

    pinkmare: modify TabLineSel highlight group

--- lua/myplugins/COLORSCHEME_pinkmare.lua
+++ lua/myplugins/COLORSCHEME_pinkmare.lua
@@ -1,7 +1,20 @@
-return { 'matsuuu/pinkmare',
+return {
+    'matsuuu/pinkmare',
     enabled = true,
     config = function()
         vim.g.pinkmare_transparent_background = true
-        vim.cmd 'colorscheme pinkmare'
-    end
+        vim.g.pinkmare_overwrite_groups = {
+            -- ['@string'] = {
+            --     fg = { '#FF38A2', '167', 'Red' },
+            --     bg = { '#FAE8B6', '223', 'White' },
+            -- },
+            -- String = {
+            --     bg = { '#87c095', '108', 'Cyan' },
+            -- },
+            TabLineSel = {
+                bg = vim.fn['pinkmare#get_palette']().bg1,
+            },
+        }
+        vim.cmd('colorscheme pinkmare')
+    end,
 }
