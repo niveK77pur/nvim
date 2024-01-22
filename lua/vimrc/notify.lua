c37a5aa94ef47a1491f5a12059932b04e1267be1
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-01-22 18:23:48 +0100

    fix(doc): level describes the minimum level

--- lua/vimrc/notify.lua
+++ lua/vimrc/notify.lua
@@ -1,6 +1,6 @@
 local old_notify = vim.notify
 
----Overwrite the default `vim.notify` to allow log level filtering. Reads maximum level from `g:notify_log_level`.
+---Overwrite the default `vim.notify` to allow log level filtering. Reads minimum level from `g:notify_log_level`.
 ---@param msg string See `h: vim.notify`.
 ---@param level integer? See `h: vim.notify`.
 ---@param opts table? See `h: vim.notify`. Forwarded to the `vim.notify()` call.
