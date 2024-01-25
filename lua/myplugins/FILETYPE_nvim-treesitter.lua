10cfe69c745888d065236a56c618dfb005387b31
niveK77pur <kevinbiewesch@yahoo.fr>
2024-01-25 11:33:07 +0100

    treesitter: remove packer.nvim workaround

--- lua/myplugins/FILETYPE_nvim-treesitter.lua
+++ lua/myplugins/FILETYPE_nvim-treesitter.lua
@@ -31,18 +31,5 @@ return {
             indent = { enable = false },
             -- textobjects = { enable = true },
         })
-
-        -- WORKAROUND
-        -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
-        --[[
-        vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-        group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-        callback = function()
-            vim.opt.foldmethod     = 'expr'
-            vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-        end
-        })
-        --]]
-        -- ENDWORKAROUND
     end,
 }
