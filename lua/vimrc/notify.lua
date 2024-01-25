823bcc9e561f3c9fa21ada0d0895c95de4cd86d2
niveK77pur <kevinbiewesch@yahoo.fr>
2024-01-25 11:25:36 +0100

    vimrc: modify default `vim.notify` to allow array of strings as message
    This should provide better compatibility in case the [`nvim-notify`](https://github.com/rcarriga/nvim-notify) plugin is not present.

--- lua/vimrc/notify.lua
+++ lua/vimrc/notify.lua
@@ -1,13 +1,17 @@
 local old_notify = vim.notify
 
 ---Overwrite the default `vim.notify` to allow log level filtering. Reads minimum level from `g:notify_log_level`.
----@param msg string See `h: vim.notify`.
+---@param msg string|string[] See `h: vim.notify`. A string list will be concatenated with newlines.
 ---@param level integer? See `h: vim.notify`.
 ---@param opts table? See `h: vim.notify`. Forwarded to the `vim.notify()` call.
 local function my_notify(msg, level, opts)
     local log_level = vim.g.notify_log_level or vim.log.levels.TRACE
     if type(level) == 'number' and level < log_level then
         return
+    end
+    ---@cast msg string[]
+    if vim.fn.type(msg) == vim.v.t_list then
+        msg = vim.fn.join(msg, [[\n]])
     end
     old_notify(msg, level, opts)
 end
