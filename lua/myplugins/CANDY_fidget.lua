1690c5e67d5513eaa011723ad4c116cbf89b70f9
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-01-31 16:02:24 +0100

    fidget: switch notifications to fidget

---
+++ lua/myplugins/CANDY_fidget.lua
@@ -0,0 +1,14 @@
+return {
+    'j-hui/fidget.nvim',
+    opts = {
+        progress = {
+            -- suppress_on_insert = true,
+        },
+        notification = {
+            override_vim_notify = true,
+            window = {
+                winblend = 0,
+            },
+        },
+    },
+}
--- lua/myplugins/LSP_nvim-lsp-notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'mrded/nvim-lsp-notify',
-    enabled = true,
-    dependencies = { 'rcarriga/nvim-notify' },
-    opts = {
-        -- notify = require('notify'),
-    },
-}
