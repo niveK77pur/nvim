23e4c055749439afc4488b79842418a80386a90a
niveK77pur <kevinbiewesch@yahoo.fr>
2024-02-04 11:08:42 +0100

    fzf.vim: do not disable mappings

--- lua/myplugins/HANDY_TOOLS_fzf_vim.lua
+++ lua/myplugins/HANDY_TOOLS_fzf_vim.lua
@@ -11,18 +11,18 @@ return {
             vim.keymap.set('i', LH, RH, args)
         end
 
-        -- -- FZF Commands (handled by ibhagwan/fzf-lua)
-        -- nmap('<Leader>fr', ':Rg<CR>')
-        -- nmap('<Leader>ff', ':Files<CR>')
-        -- nmap('<Leader>fg', ':GFiles<CR>')
-        -- nmap('<Leader>fb', ':Buffers<CR>')
-        -- nmap('<Leader>fl', ':BLines<CR>')
-        -- nmap('<Leader>fL', ':Lines<CR>')
-        -- nmap('<Leader>fh', ':Helptags<CR>')
-        -- nmap('<Leader>fm', ':Marks<CR>')
-        -- nmap('<Leader>f:', ':History:<CR>')
-        -- nmap('<Leader>f/', ':History/:<CR>')
-        -- nmap('<Leader>fs', ':Snippets:<CR>')
+        -- FZF Commands
+        nmap('<Leader>fr', ':Rg<CR>')
+        nmap('<Leader>ff', ':Files<CR>')
+        nmap('<Leader>fg', ':GFiles<CR>')
+        nmap('<Leader>fb', ':Buffers<CR>')
+        nmap('<Leader>fl', ':BLines<CR>')
+        nmap('<Leader>fL', ':Lines<CR>')
+        nmap('<Leader>fh', ':Helptags<CR>')
+        nmap('<Leader>fm', ':Marks<CR>')
+        nmap('<Leader>f:', ':History:<CR>')
+        nmap('<Leader>f/', ':History/:<CR>')
+        nmap('<Leader>fs', ':Snippets:<CR>')
 
         -- Insert mode completion
         imap('<Leader><c-x><c-k>', '<plug>(fzf-complete-word)')
