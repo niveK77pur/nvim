6321c5bc8b9eb0d57b596ad0687e2e72a03b96b3
niveK77pur <kevinbiewesch@yahoo.fr>
2024-02-04 16:26:16 +0100

    telescope: update config

--- lua/myplugins/HANDY_TOOLS_telescope.lua
+++ lua/myplugins/HANDY_TOOLS_telescope.lua
@@ -11,44 +11,76 @@ return {
             vim.keymap.set('n', LH, RH, args)
         end
         local tb = require('telescope.builtin')
+
+        -- Files ---------------------------------------------------------------
+        nmap('<leader>fr', tb.live_grep, { desc = 'Telescope: Live grep' })
         nmap('<leader>ff', function()
-            tb.find_files()
-        end)
-        nmap('<leader>fr', function()
-            tb.live_grep()
-        end)
-        nmap('<leader>fb', function()
-            tb.buffers()
-        end)
-        nmap('<leader>fl', function()
-            tb.current_buffer_fuzzy_find()
-        end)
-        nmap('<leader>fh', function()
-            tb.help_tags()
-        end)
-        nmap('<leader>fj', function()
-            tb.jumplist()
-        end)
-        nmap('<leader>fm', function()
-            tb.marks()
-        end)
-        nmap('<leader>f:', function()
-            tb.command_history()
-        end)
-        nmap('<leader>f/', function()
-            tb.search_history()
-        end)
+            tb.find_files({ no_ignore = true })
+        end, { desc = 'Telescope: Navigate files' })
+        nmap(
+            '<leader>fg',
+            tb.find_files,
+            { desc = 'Telescope: Navigate git files' }
+        )
+        nmap('<leader>fb', tb.buffers, { desc = 'Telescope: Navigate buffers' })
+        nmap(
+            '<leader>fl',
+            tb.current_buffer_fuzzy_find,
+            { desc = 'Telescope: Navigate lines in buffer' }
+        )
+        nmap('<leader>fL', function()
+            tb.live_grep({ grep_open_files = true })
+        end, {
+            desc = 'Telescope: Navigate lines in all open buffer',
+        })
+
+        -- Lists ---------------------------------------------------------------
+        nmap('<leader>fh', tb.help_tags, { desc = 'Telescope: Help tags' })
+        nmap('<leader>fj', tb.jumplist, { desc = 'Telescope: :jumps' })
+        nmap('<leader>fc', function()
+            vim.notify('Telescope cannot list :changes', vim.log.levels.ERROR)
+        end, { desc = 'Telescope: `:changes`' })
+        nmap('<leader>fm', tb.marks, { desc = 'Telescope: marks' })
+        nmap('<leader>fR', tb.registers, { desc = 'Telescope: registers' })
+        nmap(
+            '<leader>f:',
+            tb.command_history,
+            { desc = 'Telescope: command history' }
+        )
+        nmap(
+            '<leader>f/',
+            tb.search_history,
+            { desc = 'Telescope: serach history' }
+        )
         nmap('<leader>ft', function()
-            tb.treesitter()
-        end)
+            vim.notify(
+                'Telescope cannot list filter for TODOs',
+                vim.log.levels.ERROR
+            )
+        end, { desc = 'Telescope: search TODOs (TODO)' })
+        nmap(
+            '<leader>fT',
+            tb.treesitter,
+            { desc = 'Telescope: treesitter picker' }
+        )
         -- nmap('<leader>ft', function() tb.filetypes() end)
-        nmap('z=', function()
-            tb.spell_suggest()
-        end)
+
+        -- Suggestions ---------------------------------------------------------
+        nmap('z=', tb.spell_suggest, { desc = 'Telescope: spell suggestions' })
+
+        -- Insert mode completion ----------------------------------------------
+        -- TODO
+
+        -- Mapping selecting mappings ------------------------------------------
+
+        -- vim.ui.select -------------------------------------------------------
 
         -- require('telescope.sorters').get_fzy_sorter()
         -- https://github.com/nvim-telescope/telescope-fzy-native.nvim
         require('telescope').setup({
+            defaults = {
+                layout_strategy = 'flex',
+            },
             extensions = {
                 fzy_native = {
                     override_generic_sorter = true,
