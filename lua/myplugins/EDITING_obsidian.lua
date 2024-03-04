4c52b772185a86acec9929160f385c3a18d1b132
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-03-04 17:18:25 +0100

    obsidian: add plugin

---
+++ lua/myplugins/EDITING_obsidian.lua
@@ -0,0 +1,27 @@
+return {
+    'epwalsh/obsidian.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'hrsh7th/nvim-cmp',
+        'ibhagwan/fzf-lua',
+    },
+    lazy = true,
+    cond = function()
+        -- check for '.obsidian' directory in CWD
+        return not (vim.fn.empty(vim.fn.glob('.obsidian')) == 1)
+    end,
+    ft = 'markdown',
+    init = function()
+        vim.opt.conceallevel = 2
+    end,
+    opts = {
+        workspaces = {
+            {
+                -- The 'cond' to load the plugin will make sure that this
+                -- workspace points to the root of the current vault
+                name = 'current',
+                path = vim.loop.cwd(),
+            },
+        },
+    },
+}
