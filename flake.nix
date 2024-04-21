36caaf2c80db7b1d4ad0e2bcb76169ad00eba06b
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-21 23:28:16 +0200

    add nix flake devShell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake to work on my neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        stylua
+      ];
+    };
+  };
+}
