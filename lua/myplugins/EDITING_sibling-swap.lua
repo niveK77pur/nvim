ba12fba9b0df7ee5200f97b7e1afc3b3c0ffb787
niveK77pur <kevinbiewesch@yahoo.fr>
2024-05-09 11:34:15 +0200

    sibling-swap: Add description to keymaps and improve lazy loading mechanism

--- lua/myplugins/EDITING_sibling-swap.lua
+++ lua/myplugins/EDITING_sibling-swap.lua
@@ -1,16 +1,39 @@
-local keys = {
-    ['<Leader>tl'] = 'swap_with_right',
-    ['<Leader>th'] = 'swap_with_left',
-    ['<Leader>tL'] = 'swap_with_right_with_opp',
-    ['<Leader>tH'] = 'swap_with_left_with_opp',
-}
-
 return {
     'Wansmer/sibling-swap.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    keys = vim.tbl_keys(keys),
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    keys = {
+        {
+            '<Leader>tl',
+            function()
+                require('sibling-swap').swap_with_right()
+            end,
+            desc = 'sibling-swap: swap_with_right',
+        },
+        {
+            '<Leader>th',
+            function()
+                require('sibling-swap').swap_with_left()
+            end,
+            desc = 'sibling-swap: swap_with_left',
+        },
+        {
+            '<Leader>tL',
+            function()
+                require('sibling-swap').swap_with_right_with_opp()
+            end,
+            desc = 'sibling-swap: swap_with_right_with_opp',
+        },
+        {
+            '<Leader>tH',
+            function()
+                require('sibling-swap').swap_with_left_with_opp()
+            end,
+            desc = 'sibling-swap: swap_with_left_with_opp',
+        },
+    },
     opts = {
-        use_default_keymaps = true,
-        keymaps = keys,
+        use_default_keymaps = false,
     },
 }
