6e57f32fee4c30d3222763bd581ccbe781381d8a
niveK77pur <kevinbiewesch@yahoo.fr>
2024-05-09 11:40:35 +0200

    treesj: Improve lazy loading mechanism

--- lua/myplugins/EDITING_treesj.lua
+++ lua/myplugins/EDITING_treesj.lua
@@ -1,26 +1,31 @@
-local keys = {
-    toggle = '<Leader>tm',
-    join = '<Leader>tj',
-    split = '<Leader>ts',
-}
 return {
     'Wansmer/treesj',
     enabled = true,
-    keys = vim.tbl_values(keys),
+    keys = {
+        {
+            '<Leader>tm',
+            function()
+                require('treesj').toggle()
+            end,
+            desc = 'treesj: toggle',
+        },
+        {
+            '<Leader>tj',
+            function()
+                require('treesj').join()
+            end,
+            desc = 'treesj: join',
+        },
+        {
+            '<Leader>ts',
+            function()
+                require('treesj').split()
+            end,
+            desc = 'treesj: split',
+        },
+    },
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {
         use_default_keymaps = false,
     },
-    config = function()
-        local treesj = require('treesj')
-        vim.keymap.set('n', keys.toggle, treesj.toggle, {
-            desc = 'treesj - toggle',
-        })
-        vim.keymap.set('n', keys.join, treesj.join, {
-            desc = 'treesj - join',
-        })
-        vim.keymap.set('n', keys.split, treesj.split, {
-            desc = 'treesj - split',
-        })
-    end,
 }
