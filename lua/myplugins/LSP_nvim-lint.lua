83b99655889c0d474448517bea747944737b62a2
niveK77pur <kevinbiewesch@yahoo.fr>
2024-06-03 20:08:35 +0200

    feat(lilypond): Linter uses WARN loglevel

--- lua/myplugins/LSP_nvim-lint.lua
+++ lua/myplugins/LSP_nvim-lint.lua
@@ -16,6 +16,7 @@ local function lilypond_parser(openlilylib_path) --  {{{
         stdin = true,
         args = {
             string.format('--include=%s', openlilylib_path),
+            '--loglevel=WARN',
             '--output=/dev/null',
             '-dbackend=null',
             '-dno-print-pages',
