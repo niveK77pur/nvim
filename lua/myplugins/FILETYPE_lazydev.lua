41a62cc8f02d8485220c9a58ee9be7355c11b63e
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-02-01 17:04:02 +0100

    chore(lazydev): Update installation with GitHub steps

--- lua/myplugins/FILETYPE_lazydev.lua
+++ lua/myplugins/FILETYPE_lazydev.lua
@@ -7,12 +7,11 @@ return {
             library = {
                 -- See the configuration section for more details
                 -- Load luvit types when the `vim.uv` word is found
-                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
             },
         },
     },
-    { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
-    { -- optional completion source for require statements and module annotations
+    { -- optional cmp completion source for require statements and module annotations
         'hrsh7th/nvim-cmp',
         opts = function(_, opts)
             opts.sources = opts.sources or {}
@@ -22,5 +21,4 @@ return {
             })
         end,
     },
-    -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
