e5c0819193ad535d5cc9279e1ae455cedc794005
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-03 15:22:49 +0200

    feat: Set 'winborder' option

--- lua/vimrc/settings.lua
+++ lua/vimrc/settings.lua
@@ -200,6 +200,10 @@ end -- }}}
 vim.opt.foldtext = 'v:lua.MyFoldText()'
 vim.opt.fillchars = [[fold: ]]
 
+if vim.fn.has('nvim-0.11.0') then
+    vim.o.winborder = 'rounded'
+end
+
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 --                                    Editor
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
