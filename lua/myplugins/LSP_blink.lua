a823a4ff2e49c7e5144cca20b2cc751648c6ce36
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-03 15:22:49 +0200

    feat(blink): Add and configure blink plugin

---
+++ lua/myplugins/LSP_blink.lua
@@ -0,0 +1,83 @@
+return {
+    'saghen/blink.cmp',
+    enabled = function()
+        -- only enable when nvim-cmp is not enabled
+        return require('lazy.core.config').plugins['nvim-cmp'] == nil
+    end,
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'MahanRahmati/blink-nerdfont.nvim',
+        'Kaiser-Yang/blink-cmp-dictionary',
+    },
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = {
+            preset = 'default',
+        },
+
+        signature = {
+            enabled = true,
+        },
+
+        completion = {
+            keyword = {
+                range = 'full',
+            },
+        },
+
+        sources = {
+            default = {
+                'lazydev',
+                'lsp',
+                'path',
+                'snippets',
+                'nerdfont',
+                'omni',
+                'buffer',
+            },
+            per_filetype = {
+                lilypond = { 'lsp', 'snippets', 'lilypond_dict' },
+            },
+            providers = {
+                lazydev = { -- {{{1
+                    name = 'LazyDev',
+                    module = 'lazydev.integrations.blink',
+                    -- make lazydev completions top priority (see `:h blink.cmp`)
+                    score_offset = 100,
+                },
+                nerdfont = { -- {{{1
+                    module = 'blink-nerdfont',
+                    name = 'Nerd Fonts',
+                    score_offset = 15, -- Tune by preference
+                    opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
+                },
+                lilypond_dict = { -- {{{1
+                    module = 'blink-cmp-dictionary',
+                    name = 'nvim-lilpond-suite dictionary',
+                    min_keyword_length = 3,
+                    max_items = 8,
+                    opts = {
+                        dictionary_files = function()
+                            local nls_lazy = require('lazy.core.config').plugins['nvim-lilypond-suite']
+                            if nls_lazy == nil then
+                                vim.notify(
+                                    'Could not set up nvim-lilpond-suite dictionary completion source. Plugin does not seem to be loaded.',
+                                    vim.log.levels.ERROR
+                                )
+                                return
+                            end
+                            local LILYDICTPATH = nls_lazy.dir .. '/lilywords'
+                            return vim.fn.glob(LILYDICTPATH .. '/*', true, true)
+                        end,
+                    },
+                },
+                -- }}}1
+            },
+        },
+    },
+    opts_extend = { 'sources.default' },
+}
+
+-- vim: fdm=marker
