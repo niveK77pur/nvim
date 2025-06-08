bb0c4c90b276f9be5e582eb95c73d845ebf2dff3
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-08 12:44:37 +0200

    feat(nix): Implement merging of attribute under cursor
    Currently very rudimentary. It does not handle the case where the cursor
    is on a nested attribute, i.e. `x.y.z` and you have your cursor on `y`
    or `z`.
    
    It currently seems to be fetching the right values from other
    attributes, but does not compose it properly.
    
    Furthermore, it does not at all handle the case where the value of the
    first attribute is not an attribute set.

--- lua/mymodules/nix/init.lua
+++ lua/mymodules/nix/init.lua
@@ -1,7 +1,8 @@
-local M = {}
-
-local function notify(text, level)
-    vim.notify(text, level, { group = 'ftplugin-nix', annote = ':NixNestAttributes' })
+local function notify_err(text)
+    vim.notify(text, vim.log.levels.ERROR, {
+        group = 'ftplugin-nix',
+        annote = ':NixNestAttributes',
+    })
 end
 
 ---Return the attribute node at the cursor, if any. Also performs some checks on the current node.
@@ -9,11 +10,11 @@ end
 local function NixGetCursorAttributeNode() --  {{{1
     local cursor_node = vim.treesitter.get_node()
     if cursor_node == nil then
-        notify('Failed to get node at cursor', vim.log.levels.ERROR)
+        notify_err('Failed to get node at cursor')
         return
     elseif not ((cursor_node:type() == 'identifier') and (cursor_node:parent():type() == 'attrpath')) then
         -- Needs to be matching `(attrpath attr: (identifier))`
-        notify('Cusor is not on an attribute node ', vim.log.levels.ERROR)
+        notify_err('Cusor is not on an attribute node')
         return
     end
     return cursor_node
@@ -28,7 +29,7 @@ local function NixGetAttrssetScopeNode(node) -- {{{1
     repeat
         local parent = node:parent()
         if parent == nil then
-            notify('Could not find surrounding `attrset_expression` on cursor position', vim.log.levels.ERROR)
+            notify_err('Could not find surrounding `attrset_expression` on cursor position')
             return nil
         end
         node = parent
@@ -58,13 +59,117 @@ end -- }}}1
 local function NixGetAttributePathValueAfterNode(node) --  {{{
     local node_value = node:parent():next_named_sibling()
     if node_value == nil then
-        notify('Current attribute does not seem to have any value assigned', vim.log.levels.ERROR)
+        notify_err('Current attribute does not seem to have any value assigned')
         return
     end
     return node_value
 end --  }}}
 
+---Collect the entire binding expression (i.e. `x = y;`)
+---@param node TSNode
+---@return TSNode?
+local function NixGetBindingAtNode(node) --  {{{
+    local node_value = node:parent():parent()
+    if node_value == nil then
+        notify_err('Current attribute does not seem to be part of a binding/assignment')
+        return
+    end
+    return node_value
+end --  }}}
+
+local M = {}
+
 function M.nix_nest_attributes() -- {{{1
+    local buffer = vim.api.nvim_get_current_buf()
+
+    -- get text of nix attribute cursor is at
+    local cursor_node = NixGetCursorAttributeNode()
+    if cursor_node == nil then
+        return
+    end
+    local cursor_attribute_name = vim.treesitter.get_node_text(cursor_node, 0)
+
+    -- find scope of cursor in which nix attribute is found
+    local cursor_scope_node = NixGetAttrssetScopeNode(cursor_node)
+    if cursor_scope_node == nil then
+        return
+    end
+
+    -- get all other instances of the given nix attribute in the same level
+    local query = vim.treesitter.query.parse(
+        'nix',
+        string.format([[(attrpath attr: (identifier) @attribute (#eq? @attribute "%s"))]], cursor_attribute_name)
+    )
+    local query_captures = query:iter_captures(
+        cursor_scope_node,
+        buffer,
+        nil,
+        nil,
+        -- limit depth to only obtain first level attributes (not nested)
+        { max_start_depth = 3 }
+    )
+
+    -- capture all repeated nodes information for modifying the buffer
+    local _, first_node = query_captures()
+    local first_node_value = NixGetAttributePathValueAfterNode(first_node)
+    if first_node_value == nil then
+        return
+    end
+    local first_node_type = first_node_value:type()
+    local _, _, fer, fec = first_node_value:range()
+    ---@type integer[][] List of sr,sc,er,ec to be deleted from the buffer
+    local delete_list = {}
+    ---@type string[][] List of tuples of strings from other nodes to be added; 1 = attribute path, 2 = attribute value
+    local add_list = {}
+    for _, node in query_captures do
+        local node_attrs_text = NixGetAttributesAfterNode(node)
+        if node_attrs_text == nil then
+            return
+        end
+
+        local node_value = NixGetAttributePathValueAfterNode(node)
+        if node_value == nil then
+            return
+        end
+
+        local binding = NixGetBindingAtNode(node)
+        if binding == nil then
+            return
+        end
+
+        local brs, brc, bcs, bce = binding:range()
+        -- INFO: Insert in reverse to delete from bottom to top; this avoids having
+        -- to recalculate the row numbers
+        table.insert(delete_list, 1, {
+            brs,
+            brc - 1,
+            bcs,
+            bce,
+        })
+        -- INFO: Insert in reverse to insert at same position in buffer; this
+        -- avoids having to recalculate the column numbers
+        table.insert(add_list, 1, {
+            node_attrs_text,
+            vim.treesitter.get_node_text(node_value, 0),
+        })
+    end
+
+    -- delete text of other nodes
+    for _, ranges in ipairs(delete_list) do
+        local sr, sc, er, ec = unpack(ranges)
+        vim.api.nvim_buf_set_text(buffer, sr, sc, er, ec, {})
+    end
+
+    -- insert text into first node
+    for _, entries in ipairs(add_list) do
+        local attr, val = unpack(entries)
+        if first_node_type == 'attrset_expression' then
+            vim.api.nvim_buf_set_text(buffer, fer, fec - 1, fer, fec - 1, { string.format('%s = %s;', attr, val) })
+        else
+            notify_err('Unknown value type encountered for first node: ' .. first_node_type)
+            return
+        end
+    end
 end -- }}}1
 
 function M.nix_unnest_attributes() -- {{{1
