babf43381d21aecf2a199e5e9cb629e1da9c46cb
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-08 16:32:50 +0200

    fix: Prefer lazydev.nvim over deprecated neodev.lua

--- lua/myplugins/FILETYPE_lazydev.lua
+++ lua/myplugins/FILETYPE_lazydev.lua
@@ -1,24 +1,11 @@
 return {
-    {
-        'folke/lazydev.nvim',
-        ft = 'lua', -- only load on lua files
-        cond = vim.version.ge(vim.version(), { 0, 10, 0 }),
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-            },
+    'folke/lazydev.nvim',
+    enabled = function()
+        return vim.fn.has('nvim-0.10') == 1
+    end,
+    opts = {
+        library = {
+            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         },
-    },
-    { -- optional cmp completion source for require statements and module annotations
-        'hrsh7th/nvim-cmp',
-        opts = function(_, opts)
-            opts.sources = opts.sources or {}
-            table.insert(opts.sources, {
-                name = 'lazydev',
-                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-            })
-        end,
     },
 }
--- lua/myplugins/FILETYPE_neodev.lua
+++ lua/myplugins/FILETYPE_neodev.lua
@@ -1,6 +1,10 @@
 return {
     'folke/neodev.nvim',
-    enabled = true,
+    enabled = function()
+        -- only enable when lazydev.nvim is not enabled (probably using an
+        -- older nvim version here)
+        return require('lazy.core.config').plugins['lazydev.nvim'] == nil
+    end,
     cond = vim.version.lt(vim.version(), { 0, 10, 0 }),
     ft = 'lua',
     opts = {},
