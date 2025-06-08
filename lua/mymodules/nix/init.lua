ae27c42d89e5f9b9de73ba5c792a768f29ab3c41
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-08 12:44:37 +0200

    feat(nix): Function to get attribute under cursor

--- lua/mymodules/nix/init.lua
+++ lua/mymodules/nix/init.lua
@@ -4,6 +4,21 @@ local function notify(text, level)
     vim.notify(text, level, { group = 'ftplugin-nix', annote = ':NixNestAttributes' })
 end
 
+---Return the attribute node at the cursor, if any. Also performs some checks on the current node.
+---@return TSNode?
+local function NixGetCursorAttributeNode() --  {{{1
+    local cursor_node = vim.treesitter.get_node()
+    if cursor_node == nil then
+        notify('Failed to get node at cursor', vim.log.levels.ERROR)
+        return
+    elseif not ((cursor_node:type() == 'identifier') and (cursor_node:parent():type() == 'attrpath')) then
+        -- Needs to be matching `(attrpath attr: (identifier))`
+        notify('Cusor is not on an attribute node ', vim.log.levels.ERROR)
+        return
+    end
+    return cursor_node
+end -- }}}1
+
 function M.nix_nest_attributes() -- {{{1
 end -- }}}1
 
