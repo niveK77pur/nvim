d7e485a5ad588a923d3925d7f0a244b8645971ff
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-08 12:44:37 +0200

    feat(lazydev): Only load for lua filetype

--- lua/myplugins/FILETYPE_lazydev.lua
+++ lua/myplugins/FILETYPE_lazydev.lua
@@ -3,6 +3,7 @@ return {
     enabled = function()
         return vim.fn.has('nvim-0.10') == 1
     end,
+    ft = { 'lua' },
     opts = {
         library = {
             { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
