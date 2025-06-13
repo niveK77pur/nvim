e9af9efd03766f61a41614d476c1eb2b86b37c44
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-14 00:50:09 +0200

    feat(lazydev): Disable when a .luarc.json file is found

--- lua/myplugins/FILETYPE_lazydev.lua
+++ lua/myplugins/FILETYPE_lazydev.lua
@@ -8,5 +8,9 @@ return {
         library = {
             { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         },
+        enabled = function(root_dir)
+            -- disable when a .luarc.json file is found
+            return not vim.uv.fs_stat(root_dir .. '/.luarc.json')
+        end,
     },
 }
