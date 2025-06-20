09ca9af15c363f27cbdb7638e7a632f81e8e60f4
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-20 20:11:35 +0200

    feat(luasnip): Add lua snippet for vim.keymap.set

---
+++ lua/luasnip/lua.lua
@@ -0,0 +1,34 @@
+return {
+    s(
+        { trig = '(.+)map', trigEngine = 'pattern' },
+        fmta(
+            [[
+            vim.keymap.set(<mode>, '<lhs>', <rhs>, {
+                desc = '<desc>',
+                <noremap>
+            })
+            ]],
+            {
+                mode = f(function(_, snippet, _)
+                    local modes = vim.tbl_map(function(letter)
+                        return string.format("'%s'", vim.fn.list2str({ letter }))
+                    end, vim.fn.str2list(snippet.captures[1]))
+                    if #modes == 1 then
+                        return modes[1]
+                    elseif #modes > 1 then
+                        return string.format('{%s}', vim.fn.join(modes, ','))
+                    else
+                        return 'ERROR'
+                    end
+                end),
+                lhs = i(1, '<lhs>'),
+                desc = i(2, 'My Super Cool Mapping'),
+                noremap = c(3, {
+                    t('noremap = true,'),
+                    t(''),
+                }),
+                rhs = i(0, "'<rhs>'"),
+            }
+        )
+    ),
+}
