3ed937904a28a00fe46f7999250053fc47bc5e78
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-20 20:11:35 +0200

    feat(mappings): Diagnostic mapping more inline with default LSP mapping

--- lua/vimrc/mappings.lua
+++ lua/vimrc/mappings.lua
@@ -204,7 +204,7 @@ if vim.version.lt(vim.version(), { 0, 10, 0 }) then
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { unpack(diagnostic_opts), desc = 'Go to previous diagnostic' })
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { unpack(diagnostic_opts), desc = 'Go to next diagnostic' })
 end
-vim.keymap.set('n', '<Leader>lq', vim.diagnostic.setloclist, {
+vim.keymap.set('n', 'gdl', vim.diagnostic.setloclist, {
     unpack(diagnostic_opts),
     desc = 'Add buffer diagnostics to location list',
 })
