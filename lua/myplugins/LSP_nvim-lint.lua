db9263c2ed7f669a8b7007ce79dedd4ed2f78224
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-27 20:03:30 +0200

    feat(nvim-lint): Remove lilypond config in favour of nvim-lilypond-suite's diagnostics

--- lua/myplugins/LSP_nvim-lint.lua
+++ lua/myplugins/LSP_nvim-lint.lua
@@ -1,40 +1,7 @@
 local linters = {
     sh = { 'shellcheck' },
-    lilypond = { 'lilypond' },
     nix = { 'statix' },
 }
-
-local function lilypond_parser(openlilylib_path) --  {{{
-    local pattern = '^-:(%d+):(%d+): (%w+): (.*)'
-    local groups = { 'lnum', 'col', 'severity', 'message' }
-    local severity_map = {
-        ['error'] = vim.diagnostic.severity.ERROR,
-        ['warning'] = vim.diagnostic.severity.WARN,
-    }
-
-    return {
-        cmd = 'lilypond',
-        stdin = true,
-        args = {
-            string.format('--include=%s', openlilylib_path),
-            '--loglevel=WARN',
-            '--output=/dev/null',
-            '-dbackend=null',
-            '-dno-print-pages',
-            '-dcompile-scheme-code',
-            '-dno-aux-files',
-            '-dno-include-eps-fonts',
-            '-dno-include-book-title-preview',
-            '-dno-point-and-click',
-            '-',
-        },
-        stream = 'stderr',
-        ignore_exitcode = true,
-        parser = require('lint.parser').from_pattern(pattern, groups, severity_map, {
-            severity = vim.diagnostic.severity.INFO,
-        }),
-    }
-end --  }}}
 
 return {
     'mfussenegger/nvim-lint',
@@ -42,8 +9,6 @@ return {
     ft = vim.tbl_keys(linters),
     config = function()
         local lint = require('lint')
-
-        lint.linters.lilypond = lilypond_parser('./openlilylib')
 
         lint.linters_by_ft = linters
         vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
