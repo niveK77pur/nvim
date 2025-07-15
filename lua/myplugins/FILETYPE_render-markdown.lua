d5d9a64827e323d723f6013269d52826497b187d
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-15 10:15:38 +0200

    feat(render-markdown): Add plugin

---
+++ lua/myplugins/FILETYPE_render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    enabled = true,
+    ft = { 'markdown' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        completions = {
+            blink = { enabled = true },
+            lsp = { enabled = true },
+        },
+    },
+}
