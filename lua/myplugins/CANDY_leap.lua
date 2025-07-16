c849e163c1c42d24838bee7c082da9a41c77ccd3
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-16 09:07:43 +0200

    feat(leap): Clean up and update configuration

--- lua/myplugins/CANDY_leap.lua
+++ lua/myplugins/CANDY_leap.lua
@@ -2,32 +2,9 @@ return {
     'ggandor/leap.nvim',
     enabled = true,
     config = function()
-        local l = require('leap')
+        require('leap').set_default_mappings()
         vim.schedule(function()
             vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Conceal' })
         end)
-        -- l.set_default_keymaps(true)
-        l.add_default_mappings()
-
-        -- target_windows = { vim.fn.win_getid() }
-        -- require('leap').leap { target_windows = { vim.fn.win_getid() } }
-        -- require('leap').leap { target_windows = vim.tbl_filter(
-        --   function (win) return vim.api.nvim_win_get_config(win).focusable end,
-        --   vim.api.nvim_tabpage_list_wins(0)
-        -- )}
-
-        -- Bidirectional search. Initiate multi-window mode with the current
-        -- window as the only target.
-        -- vim.keymap.set(
-        --     { "n" }, "s",
-        --     function()
-        --         require('leap').leap({ target_windows = { vim.fn.win_getid() } })
-        --     end,
-        --     {
-        --         desc = "Bidirectional search. Initiate " ..
-        --             "multi-window mode with the current window as the only " ..
-        --             "target"
-        --     }
-        -- )
     end,
 }
