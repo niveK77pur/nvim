13f0fc7276d910d18cdaf59b3d9b20655ad51e15
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-08-09 12:14:51 +0100

    feat(everforest): Update colorscheme configuration

--- lua/myplugins/COLORSCHEME_everforest.lua
+++ lua/myplugins/COLORSCHEME_everforest.lua
@@ -2,9 +2,11 @@ return {
     'sainnhe/everforest',
     enabled = true,
     init = function()
-        vim.g.everforest_transparent_background = 1
-        -- treesitter highlighting seem to interfere with everforest
-        pcall(vim.cmd, 'TSDisable highlight')
+        vim.g.everforest_enable_italic = true
+        vim.g.everforest_better_performance = true
+        vim.g.everforest_dim_inactive_windows = true
+        -- vim.g.everforest_diagnostic_text_highlight = true
+        vim.g.everforest_diagnostic_virtual_text = 'highlighted'
     end,
     config = function()
         vim.cmd([[colorscheme everforest]])
