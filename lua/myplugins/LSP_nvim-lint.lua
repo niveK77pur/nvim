1ad98851b3d3c57207652df259a7165da0eb6cfe
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-08-11 13:14:05 +0100

    refactor(nvim-lint): Rename linters variable

--- lua/myplugins/LSP_nvim-lint.lua
+++ lua/myplugins/LSP_nvim-lint.lua
@@ -1,4 +1,4 @@
-local linters = {
+local linters_by_ft = {
     sh = { 'shellcheck' },
     nix = { 'statix' },
 }
@@ -6,11 +6,11 @@ local linters = {
 return {
     'mfussenegger/nvim-lint',
     enabled = true,
-    ft = vim.tbl_keys(linters),
+    ft = vim.tbl_keys(linters_by_ft),
     config = function()
         local lint = require('lint')
 
-        lint.linters_by_ft = linters
+        lint.linters_by_ft = linters_by_ft
         vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
             callback = function()
                 lint.try_lint()
