58f3e8c6e7a53a5dd1fa730237bf54fe586b861c
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-11-30 09:02:49 +0100

    feat(nvim-treesitter): Update to main branch

--- lua/myplugins/FILETYPE_nvim-treesitter.lua
+++ lua/myplugins/FILETYPE_nvim-treesitter.lua
@@ -1,51 +1,51 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     enabled = true,
-    build = function()
-        require('nvim-treesitter.install').update({ with_sync = true })
-        -- vim.cmd([[TSUpdate]])
-    end,
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup({ --  {{{1
-            -- A list of parser names, or "all"
-            -- ensure_installed = { "c", "lua", "rust" },
+        local augroup_treesitter = vim.api.nvim_create_augroup('treesitter', {})
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-            -- Automatically install missing parsers when entering buffer
-            auto_install = true,
-            -- List of parsers to ignore installing (for "all")
-            -- ignore_install = { "javascript" },
-
-            --If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-            highlight = {
-                enable = true,
-                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is the name of the parser)
-                disable = {
-                    'latex',
-                },
-            },
-            incremental_selection = { enable = true },
-            indent = { enable = false },
-            -- textobjects = { enable = true },
+        vim.api.nvim_create_autocmd({ 'FileType' }, {
+            group = augroup_treesitter,
+            pattern = vim.tbl_keys(require('nvim-treesitter.parsers')),
+            callback = function()
+                vim.treesitter.start()
+            end,
         })
-        ---@diagnostic disable-next-line: inject-field
-        -- parser_config.lilypond = { --  {{{1
-        --     install_info = {
-        --         url = 'https://github.com/tristanperalta/tree-sitter-lilypond',
-        --         files = { 'src/parser.c' },
-        --         -- optional entries:
-        --         -- branch = 'main', -- default branch in case of git repo if different from master
-        --         -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        --         -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-        --     },
-        --     -- filetype = 'lilypond', -- if filetype does not match the parser name
-        -- }
-        --  }}}1
+
+        vim.api.nvim_create_autocmd('FileType', {
+            group = augroup_treesitter,
+            pattern = 'tex',
+            callback = function(args)
+                vim.treesitter.start(args.buf)
+                vim.bo[args.buf].syntax = 'on' -- only if additional legacy syntax is needed
+            end,
+        })
+
+        -- Custom parsers
+        vim.api.nvim_create_autocmd('User', {
+            group = augroup_treesitter,
+            pattern = 'TSUpdate',
+            callback = function()
+                local lilypond_install_info = { --  {{{1
+                    url = 'https://github.com/nwhetsell/tree-sitter-lilypond',
+                    revision = '23eb50341020381521c5bc7f6895dc50ab482b25',
+                    location = 'lilypond',
+                    queries = 'queries',
+                }
+                require('nvim-treesitter.parsers').lilypond = { --  {{{1
+                    install_info = lilypond_install_info,
+                    tier = 3,
+                }
+                require('nvim-treesitter.parsers')['lilypond-scheme'] = { --  {{{1
+                    install_info = vim.tbl_extend('force', lilypond_install_info, { location = 'lilypond-scheme' }),
+                    tier = 3,
+                }
+                --  }}}1
+            end,
+        })
     end,
 }
 
