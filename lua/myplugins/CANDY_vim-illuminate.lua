b2631116cdf1cc01f1261efc2aa6a6daa0c86a1d
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-12-08 10:23:08 +0100

    fix(vim-illuminate): Collision with trailblazer on <a-p>

--- lua/myplugins/CANDY_vim-illuminate.lua
+++ lua/myplugins/CANDY_vim-illuminate.lua
@@ -1,11 +1,27 @@
 return {
     'RRethy/vim-illuminate',
     enabled = true,
+    lazy = false,
+    keys = {
+        {
+            '<a-n>',
+            function()
+                require('illuminate').goto_next_reference()
+            end,
+        },
+        {
+            '<a-N>',
+            function()
+                require('illuminate').goto_prev_reference()
+            end,
+        },
+    },
     config = function()
         require('illuminate').configure({
             filetypes_denylist = { 'lilypond' },
             modes_denylist = { 'i' },
             min_count_to_highlight = 2,
+            disable_keymaps = true,
         })
     end,
 }
