5576ad8a77a8b43a6c7840a262da5abf7d9a6597
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-12-22 22:16:13 +0100

    fix(instant.nvim): Saving joined buffers breaks the syncing
    Note that this patch cannot discern between client and server. Ideally,
    on the server we would like to keep saving the buffers. However, on the
    client, saving the buffer may lead to issues in that the syncing breaks.
    
    As a slight inconvenience for the server, but to prevent syncing issues
    with the client, both will have their buffers in readonly.

--- lua/myplugins/COLLABORATION_instant.lua
+++ lua/myplugins/COLLABORATION_instant.lua
@@ -10,5 +10,25 @@ return {
     },
     config = function()
         vim.g.instant_username = 'MaceVimdu'
+
+        local augroup_instant = vim.api.nvim_create_augroup('instant', {})
+        vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufEnter' }, {
+            group = augroup_instant,
+            pattern = { '*' },
+            desc = 'Prevent saving instant.nvim buffers',
+            callback = function(args)
+                -- If the client saves the synced buffer, then the syncing
+                -- seems to break and the server only receives partial updates
+                -- or junk.
+                if vim.tbl_contains(require('instant').get_connected_buf_list(), args.buf) then
+                    vim.notify(
+                        string.format('Buffer %s should not be saved [%s]', args.buf, args.file),
+                        vim.log.levels.WARN,
+                        { title = 'instant.nvim', group = 'instant.nvim' }
+                    )
+                    vim.bo[args.buf].readonly = true
+                end
+            end,
+        })
     end,
 }
