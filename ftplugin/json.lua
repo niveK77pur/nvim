ae7c8e8f1a0fb14e3b4c662f4da231e9bfa9e829
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-01-27 09:00:33 +0100

    fix(ftplugin/json): appwrite.config.json uses 4 spaces indentation

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,1 +1,11 @@
 vim.opt.tabstop = 2
+
+local augroup_ftplugin_json = vim.api.nvim_create_augroup('ftplugin_json', {})
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    group = augroup_ftplugin_json,
+    pattern = { 'appwrite.config.json' },
+    desc = 'appwrite json configuration',
+    callback = function()
+        vim.bo.tabstop = 4
+    end,
+})
