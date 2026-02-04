e543d4adf2809f340c24aea2e398c3198b7dad8a
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-02-04 14:45:24 +0100

    feat(multiterm): Add plugin

---
+++ lua/myplugins/HANDY_TOOLS_multiterm.lua
@@ -0,0 +1,10 @@
+return {
+    'imranzero/multiterm.nvim',
+    event = 'VeryLazy',
+    opts = {
+        keymaps = {
+            toggle = '<F12>',
+            list = '<leader><F12>',
+        },
+    },
+}
