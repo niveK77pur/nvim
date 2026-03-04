85f75b64f0b169e6d8a1af1b9f1399a5cad326a0
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-03-04 22:46:47 +0100

    fix(colorizer): Needs `termguicolors` option

--- lua/myplugins/CANDY_nvim-colorizer.lua
+++ lua/myplugins/CANDY_nvim-colorizer.lua
@@ -1,6 +1,9 @@
 return {
     'NvChad/nvim-colorizer.lua',
     enabled = true,
+    init = function()
+        vim.cmd('set termguicolors')
+    end,
     config = function()
         require('colorizer').setup({
             mode = ({ 'foreground', 'background', 'virtualtext' })[3],
