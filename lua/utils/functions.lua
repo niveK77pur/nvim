4f20c38a6e8c80675a05fa8a7c1bdc45b12bb987
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-07 21:04:04 +0200

    chore(utils): Remove currently unused functions

--- lua/utils/functions.lua
+++ lua/utils/functions.lua
@@ -1,16 +1,6 @@
 local myfs = {}
 
-function myfs.tstestfunc()
-    local v = 'yo'
-    local function helper(t)
-        return ('>%s<'):format(t)
-    end
-    local h = function(t)
-        return ('<%s>'):format(t)
-    end
-    return h(helper(v))
-end
-
+---@deprecated Prefer vim.ui.select
 function myfs.nuiInput(params)
     params.position = params.position or '50%'
     params.width = params.width or 30
@@ -64,6 +54,7 @@ function myfs.nuiInput(params)
     input:mount()
 end
 
+---@deprecated
 function myfs.getvpos()
     --[[
         Inspired by https://github.com/ibhagwan/nvim-lua/blob/main/lua/utils.lua#L89
@@ -88,50 +79,6 @@ function myfs.getvpos()
     return v_start, v_end
 end
 
-function myfs.GetCommentCharacter()
-    -- add stuff before and after comment character
-    local pre, post = '', ''
-    if vim.o.filetype == 'python' then
-        post = ' '
-    end
-    for _, v in pairs(vim.fn.split(vim.o.comments, ',')) do
-        if v:match('^b?:') then
-            return pre .. v:match(':(.*)') .. post
-        end
-    end
-    return pre .. '' .. post
-end
-
-function myfs.MakeHeader(text)
-    local indent = vim.fn.substitute(vim.fn.getline('.'), [[^\s*\zs\S.*]], '', '')
-    local comment_character = myfs.GetCommentCharacter()
-    local textwidth = (vim.o.textwidth > 0) and vim.o.textwidth or 80
-    local width = textwidth - #indent - #comment_character
-    local space = (' '):rep(vim.fn.round(width / 2 - #text / 2))
-
-    local banner = ('%s%s%s'):format(indent, comment_character, ('~'):rep(width))
-    local text_line = ('%s%s%s%s'):format(indent, comment_character, space, text)
-    vim.fn.append('.', {
-        banner,
-        text_line,
-        banner,
-    })
-end
-
-function myfs.MakeSection(text)
-    local indent = vim.fn.substitute(vim.fn.getline('.'), [[^\s*\zs\S.*]], '', '')
-    local comment_character = myfs.GetCommentCharacter()
-    local textwidth = (vim.o.textwidth > 0) and vim.o.textwidth or 80
-    local width = textwidth - #indent - #comment_character
-    text = (' %s '):format(text)
-
-    local banner = ('-'):rep(width - #text)
-    local text_line = ('%s%s%s%s'):format(indent, comment_character, text, banner)
-    vim.fn.append('.', {
-        text_line,
-    })
-end
-
 function myfs.plugin_loaded(plugin_name)
     return myfs.lazy_plugin_loaded(plugin_name)
     -- return myfs.packer_plugin_loaded(plugin_name)
@@ -140,18 +87,6 @@ end
 function myfs.lazy_plugin_loaded(plugin_name)
     return require('lazy.core.config').plugins[plugin_name]
         and require('lazy.core.config').plugins[plugin_name]._.loaded
-end
-
-function myfs.packer_plugin_loaded(plugin_name)
-    return packer_plugins[plugin_name] and packer_plugins[plugin_name].loaded
-end
-
-function myfs.return_gathered_plugins(require_path, require_names)
-    local plugin_specs = {}
-    for _, r in ipairs(require_names) do
-        table.insert(plugin_specs, require(string.format('%s.%s', require_path, r)))
-    end
-    return plugin_specs
 end
 
 return myfs
