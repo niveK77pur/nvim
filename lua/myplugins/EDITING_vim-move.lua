856d1f1c442bb4b232f4448f0e06f8852386c8fb
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-07 21:07:26 +0200

    chore: Delete unused EDITING plugins

--- lua/myplugins/EDITING_vim-move.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'matze/vim-move',
-    enabled = false,
-    config = function()
-        -- vim.g.move_key_modifier = 'S-A'
-        -- vim.g.move_key_modifier_visualmode = 'S-A'
-    end,
-}
