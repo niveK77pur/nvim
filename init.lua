0dd64094f80e647c24e9463ceead6baab7387979
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-08 10:15:24 +0200

    fix(init): Absolute path to vimrc configuration

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[  ]]
 
-for fname, type in vim.fs.dir('lua/vimrc', { depth = math.huge }) do
+for fname, type in vim.fs.dir(vim.fn.stdpath('config') .. '/lua/vimrc', { depth = math.huge }) do
     if type == 'file' and vim.fs.ext(fname) == 'lua' then
         local name = string.gsub(fname, '%.lua$', '')
         name = string.gsub(name, '/', '.')
