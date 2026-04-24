2a1ac398e55b6d9f99f791f36a15f874cf4187d4
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-24 19:12:21 +0200

    fix: Allow specifying loading order of vimrc files

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[  ]]
 
-for fname, type in vim.fs.dir(vim.fn.stdpath('config') .. '/lua/vimrc', { depth = math.huge }) do
-    if type == 'file' and vim.fs.ext(fname) == 'lua' then
-        local name = string.gsub(fname, '%.lua$', '')
+local vimrc_path = vim.fn.stdpath('config') .. '/lua/vimrc'
+
+for _, fname in ipairs(vim.fn.glob(vimrc_path .. '/**/*.lua', false, true)) do
+    local name = vim.fs.relpath(vimrc_path, fname)
+    if name == nil then
+        print('Failed to get relative path for vimrc file: ' .. fname)
+    else
+        name = string.gsub(name, '%.lua$', '')
         name = string.gsub(name, '/', '.')
         require('vimrc.' .. name)
     end
