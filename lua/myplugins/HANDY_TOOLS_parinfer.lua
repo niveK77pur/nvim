4c2f09b74c9f68b8d262bf0e442adcce9e06e6be
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-06-20 11:09:10 +0200

    feat(parinfer): Update list of filetypes to load on

--- lua/myplugins/HANDY_TOOLS_parinfer.lua
+++ lua/myplugins/HANDY_TOOLS_parinfer.lua
@@ -7,5 +7,18 @@ return {
     end,
     'eraserhd/parinfer-rust',
     build = 'cargo build --release',
-    ft = { 'yuck' },
+    ft = {
+        -- https://github.com/eraserhd/parinfer-rust/blob/7f13acea20f03228f72156cec4fa4e49f0d5d1e5/plugin/parinfer.vim#L310-L317
+        'clojure',
+        'scheme',
+        'lisp',
+        'racket',
+        'hy',
+        'fennel',
+        'janet',
+        'carp',
+        'wast',
+        'yuck',
+        'dune',
+    },
 }
